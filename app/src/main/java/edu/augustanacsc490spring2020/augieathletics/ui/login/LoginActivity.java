f86feeb7ec935d24a02136391ab622d3a63968ff
brysonadcock22 <brysonadcock16@augustana.edu>
2020-04-20 19:37:19 -0500

    Added ability to send user to main window after successful login

--- app/src/main/java/edu/augustanacsc490spring2020/augieathletics/ui/login/LoginActivity.java
+++ app/src/main/java/edu/augustanacsc490spring2020/augieathletics/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import edu.augustanacsc490spring2020.augieathletics.MainActivity;
 import edu.augustanacsc490spring2020.augieathletics.R;
 import edu.augustanacsc490spring2020.augieathletics.ui.login.LoginViewModel;
 import edu.augustanacsc490spring2020.augieathletics.ui.login.LoginViewModelFactory;
@@ -122,6 +124,8 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
