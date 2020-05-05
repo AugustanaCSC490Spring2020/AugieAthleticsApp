afa879f3de1a8a43fa2081121d3c4ff07862f314
brysonadcock22 <brysonadcock16@augustana.edu>
2020-05-05 11:27:14 -0500

    Code Review fixed login messiness

---
+++ app/src/main/java/edu/augustanacsc490spring2020/augieathletics/IntroActivity.java
@@ -0,0 +1,14 @@
+package edu.augustanacsc490spring2020.augieathletics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
